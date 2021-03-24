c2cecdbe76aa9ea5b4a861465d7d2b1016b4c640
apoloEafit <vaarcilal@eafit.edu.co>
2021-03-24 18:47:46 -0500

    scripts for running the compilations and the slurm scripts

---
+++ compilations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# FFTW
+cd /root/PRESTO-automatization
+./fftw.sh
+
+# CFITSIO
+cd /root/PRESTO-automatization
+./cfitsio.sh
+
+# TEMPO
+cd /root/PRESTO-automatization
+./tempo.sh
+
+# GLIB
+
+# PGPLOT
+
+# PRESTO
---
+++ launch_presto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TEST DATA 1
+cd /root/PRESTO-automatization
+./sbatch_prestoTD1.sh
+
+# TEST DATA 2
+cd /root/PRESTO-automatization
+./sbatch_prestoTD2.sh
