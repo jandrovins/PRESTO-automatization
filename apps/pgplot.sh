f13dfa2026ff69c71011c20242d3eeb36a512d69
mherreral <mherreral@eafit.edu.co>
2021-03-24 23:20:31 -0500

    Added pgplot compilation

--- apps/pgplot.sh
+++ apps/pgplot.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+
+DIS_DIR=$(pwd)/files/pgplot
+SRC_DIR=$DIS_DIR/pgplot-5.2
+TARGET_DIR='/share/apps/pgplot/gcc-8.3.1'
+CORES=16
+
+tar xf $DIS_DIR/pgplot-5.2.tar.gz
+mkdir -p $TARGET_DIR
+cp $DIS_DIR/drivers.list $DIS_DIR/local.conf $TARGET_DIR
+cd $TARGET_DIR
+
+$SRC_DIR/makemake $SRC_DIR linux #uses $TARGET_DIR/local.conf as third
+
+make -j $CORES
+make clean
+make cpg
+
+cp -r ./modules/pgplot /share/apps/modules/
